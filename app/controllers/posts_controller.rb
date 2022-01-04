2c40fa7dcf0f47294255499cd544fe7dbdf11541
akash <kumar.ryan94@gmail.com>
2022-01-04 14:12:31 +0530

    Add post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+#define strong parameter
+  private
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
